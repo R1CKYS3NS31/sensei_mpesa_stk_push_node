72305faef57d454e183600001d742ed9b4bd8883
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-11-07 08:39:00 +0300

    /mpesa_api work on lipa_na_mpesa_stk_push  commit

---
+++ sensei_mpesa_api_nodejs/README.md
@@ -0,0 +1,20 @@
+
+```bash
+$ npm install
+```
+
+- Go through the `index.md` file.
+
+- Enter your credentials on the `.env` file.
+
+- Start the server:
+
+```bash
+$ npm run dev
+```
+
+- Start Ngrok
+
+```bash
+$ npm run ngrok
+```
---
+++ sensei_mpesa_api_nodejs/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || process.env.APP_PORT;
+
+//app configurations
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+//route.
+const mpesa = require("./routes/index");
+
+//listening to a specific route
+app.use("/mpesa", mpesa);
+
+//listening to a port.
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
